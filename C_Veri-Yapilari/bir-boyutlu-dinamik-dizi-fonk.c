5d95e1a72370eec8071aaa0dd64cdad30b680e10
Harun Uyguç <113466382+HarunUYGUC@users.noreply.github.com>
2023-10-18 14:07:28 +0300

    Bir Boyutlu Dinamik Dizi Fonksiyonu

---
+++ C_Veri-Yapilari/bir-boyutlu-dinamik-dizi-fonk.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void bir_boyutlu_matris_olustur(int** ptr, int sutunSayisi)
+{
+	*ptr = (int*)malloc(sizeof(int) * sutunSayisi);
+
+	for (int i = 0; i < sutunSayisi; i++)
+	{
+		*(*ptr + i) = rand() % 100;
+		printf("%d \t", *(*ptr + i));
+	}
+}
+
+int main()
+{
+	int sutunSayisi;
+	int* ptr;
+
+	printf("Sütun sayýsýný giriniz: ");
+	scanf_s("%d", &sutunSayisi);
+
+	bir_boyutlu_matris_olustur(&ptr, sutunSayisi);
+
+	return 0;
+}
