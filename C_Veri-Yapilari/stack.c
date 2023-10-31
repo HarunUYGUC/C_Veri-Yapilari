7b79fdf71438a9bc1e6df1dd640edc12a8ff683c
Harun Uyguç <113466382+HarunUYGUC@users.noreply.github.com>
2023-10-31 18:53:44 +0300

    Stack

---
+++ C_Veri-Yapilari/stack.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Stack
+struct node
+{
+	int data;
+	struct node* next;
+};
+
+// Top
+struct node* top = NULL;
+
+void push(int data)
+{
+	// Stack is empty.
+	if (top == NULL)
+	{
+		struct node* new = (struct node*)malloc(sizeof(struct node));
+		new->data = data;
+		new->next = NULL;
+
+		top = new;
+	}
+	// Stack isn't empty.
+	else
+	{
+		struct node* new = (struct node*)malloc(sizeof(struct node));
+		new->data = data;
+		new->next = top;
+
+		top = new;
+	}
+}
+
+// Display
+int display()
+{
+	// Stack is empty.
+	if (top == NULL)
+	{
+		printf("Stack is empty. \n");
+		return 1;
+	}
+
+	// Stack isn't empty.
+	struct node* index = top;
+
+	while (index != NULL)
+	{
+		printf("%d - ", index->data);
+		index = index->next;
+	}
+
+	printf("\n");
+
+	return 1;
+}
+
+// Pop
+int pop()
+{
+	// Stack is empty.
+	if (top == NULL)
+	{
+		printf("Stack is empty. \n");
+		return 1;
+	}
+
+	// Stack isn't empty.
+	struct node* temp = top;
+
+	top = top->next;
+
+	free(temp);
+
+	return 1;
+}
+
+int main()
+{
+	push(1);
+	push(2);
+	push(4);
+	display();
+	printf("------------ \n");
+
+	pop();
+	display();
+	printf("------------ \n");
+
+	pop();
+	display();
+	printf("------------ \n");
+
+	pop();
+	display();
+	printf("------------ \n");
+
+	push(7);
+	push(9);
+	push(13);
+	display();
+	printf("------------ \n");
+
+	return 1;
+}
